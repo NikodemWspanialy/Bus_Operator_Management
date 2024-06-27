f89a168c00159dfc36ab08b5f385f1865be8363f
Kamil Nabożny <91849080+Quantoreq@users.noreply.github.com>
2024-06-27 17:49:43 +0200

    Delete frontend/src directory

--- frontend/src/pages/Logowanie.jsx
+++
@@ -1,5 +0,0 @@
-function Logowanie() {
-    return <h1>Logowanie</h1>;
-  };
-  
-  export default Logowanie;
--- frontend/src/pages/PanelKursow.jsx
+++
@@ -1,5 +0,0 @@
-function PanelKursow() {
-    return <h1>Panel Kursow</h1>;
-};
-
-export default PanelKursow;
